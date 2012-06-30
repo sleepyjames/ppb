8aa654475d3e0b50d5bae6613f8a446b9e94caa5
Martin Allison <martin@potatolondon.com>
2012-06-30 01:22:51 +0100

    URLs to snippets views

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,5 +3,11 @@ from django.conf.urls.defaults import patterns, url, include
 urlpatterns = patterns(
     'snippets',
     url(r'^$', 'views.home', {}, name='home'),
+    url(r'^snippet/(?P<snippet_id>\d+)/$', 'views.snippet_detail', {},
+        name='snippet-detail'),
+    url(r'^snippet/new/$', 'views.edit_snippet', {'snippet_id':None},
+        name='new-snippet'),
+    url(r'^snippet/(?P<snippet_id>\d+)/edit/$', 'views.edit_snippet', {},
+        name='edit-snippet'),
 )
 
