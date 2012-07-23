bbcb0a1f5612846915cdadcf7bccba7fb0d741af
Martin Allison <martin@potatolondon.com>
2012-07-24 00:15:05 +0100

    Get rid of default kwargs for new-snippet URL

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,8 +5,7 @@ urlpatterns = patterns(
     url(r'^$', 'views.home', {}, name='home'),
     url(r'^snippet/(?P<snippet_id>\d+)/$', 'views.snippet_detail', {},
         name='snippet-detail'),
-    url(r'^snippet/new/$', 'views.edit_snippet', {'snippet_id':None},
-        name='new-snippet'),
+    url(r'^snippet/new/$', 'views.new_snippet', {}, name='new-snippet'),
     url(r'^snippet/(?P<snippet_id>\d+)/edit/$', 'views.edit_snippet', {},
         name='edit-snippet'),
 )
