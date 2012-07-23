ba71f2b663682ea43e8dd0fe3b567acd03b0eaf7
Martin Allison <martin@potatolondon.com>
2012-07-24 00:18:40 +0100

    Views refactoring, search mixin, split edit snippet and add snippet out into different views

--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,7 @@
 import logging
+from datetime import datetime
 
 from google.appengine.ext import db
-from google.appengine.api import users
 
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
@@ -24,26 +24,45 @@ def get_snippet_or_404_on_error(snippet_id):
         snippet_id = int(snippet_id)
     except (TypeError, ValueError):
         raise Http404()
-    
+
     return CodeSnippet.get_by_id(snippet_id)
 
 
-def get_current_user():
-    "Quick implementation for getting the current user object"
-    # TODO: do this properly
+def get_snippet_or_404(snippet_id):
+    snippet = get_snippet_or_404_on_error(snippet_id)
+    if snippet is None:
+        raise Http404()
+    return snippet
 
-    u = users.get_current_user()
-    if not u:
-        return u
 
-    id = u.user_id()
-    email = u.email()
+def index_snippet_with_search(snippet):
+    snippet_doc = CodeSnippetDocument(
+        doc_id=snippet.key().id(),
+        title=snippet.title,
+        code=snippet.code,
+        language_id=snippet.language,
+        language_readable=snippet.get_language(),
+        creator_email=snippet.creator.email,
+        created=datetime.now().date(),
+        modified=snippet.modified.date(),
+    )
+    index = Index(name='snippets')
+    index.add(snippet_doc)
 
-    db_u = User.get_by_key_name(str(id))
-    if db_u is None:
-        db_u = User(key_name=str(id), email=email)
-        db_u.put()
-    return db_u
+
+def copy_snippet_from_form(form, snippet=None):
+    if snippet is None:
+        snippet = CodeSnippet()
+
+    for field_name, clean_val in form.cleaned_data.items():
+        setattr(snippet, field_name, clean_val)
+
+    current_user = User.get_current_user()
+    if not snippet.is_saved():
+        snippet.creator = current_user
+    snippet.modifier = current_user
+
+    return snippet
 
 
 def _reindex(request):
@@ -75,125 +94,164 @@ def _purge(request):
     return HttpResponse('Done')
 
 
-class Home(TemplateView):
+class SearchMixin(object):
+    """Will add search results of the given parameters to the view that
+    inherits it from it.
+    """
+    # Configuration for searching
+
+    # Default max number of items to return
+    results_limit = 5
+
+    # Index name to search and document class to use for results
+    index_name = None
+    document_class = None
+
+    # URL param name for keyword search - is also the context name for the
+    # keywords in the search
+    query_param_name = 'q'
+    # Allowed filter URL param names
+    allowed_filters = {}
+
+    # Context names for the list of results and the applied filters
+    results_context_name = 'results'
+    filters_context_name = 'filters'
+
+    def get_filters(self):
+        """Returns a dict of filters to apply to the search"""
+        filters = {}
+        for k, v in self.request.GET.items():
+            if k in self.allowed_filters:
+                filters[k] = v
+        return filters
+
+    def get_keywords(self):
+        """Gets the keywords for the search"""
+        return self.request.GET.get(self.query_param_name, '')
+
+    def get_context_data(self, **kwargs):
+        ctx = super(SearchMixin, self).get_context_data(**kwargs)
+
+        query = None
+        results_count_context_name = self.results_context_name+'_count'
+
+        filters = self.get_filters()
+        q = self.get_keywords()
+
+        # The API wrapper should possibly handle the need for the following ifs
+        if q or filters:
+            query = Index(name=self.index_name).search(self.document_class)
+
+        if query:
+            if q:
+                query = query.keywords(q)
+            if filters:
+                query = query.filter(**filters)
+
+            ctx[self.results_context_name] = query[:self.results_limit]
+            ctx[results_count_context_name] = query.count()
+
+        ctx[self.query_param_name] = q
+        ctx[self.filters_context_name] = filters
+        ctx.setdefault(self.results_context_name, [])
+        ctx.setdefault(results_count_context_name, 0)
+
+        return ctx
+
+
+class Home(SearchMixin, TemplateView):
     """Shows the 5 latest snippets along with the 5 latest search results for
     any search if one has been performed.
     """
     template_name = 'snippets/index.html'
+    index_name = 'snippets'
+    document_class = CodeSnippetDocument
+    allowed_filters = ['language_id']
 
     def get_context_data(self, **kwargs):
         ctx = super(Home, self).get_context_data(**kwargs)
-
-        # Mainly for testing my search API wrapper
-        q = self.request.GET.get('q', '')
-        if q:
-            query = Index(name='snippets').search(CodeSnippetDocument)
-            query = query.keywords(q)
-            ctx['results'] = query[:5]
-            ctx['results_count'] = query.count()
-
-        ctx.setdefault('results', [])
-        ctx.setdefault('results_count', 0)
-        
-        ctx['q'] = q
         ctx['latest_snippets'] = CodeSnippet.all().order('modified').fetch(5)
-
         return ctx
 
 home = Home.as_view()
 
 
 class SnippetDetail(TemplateView):
-    "Detail view for a single snippet"
+    "Detail view for a single snippet along with its last 5 comments"
     template_name = 'snippets/detail.html'
 
     def get_context_data(self, **kwargs):
-        code_snippet = get_snippet_or_404_on_error(self.kwargs.get('snippet_id'))
-
-        if code_snippet is None:
-            raise Http404()
+        code_snippet = get_snippet_or_404(self.kwargs.get('snippet_id'))
 
         ctx = super(SnippetDetail, self).get_context_data(**kwargs)
         ctx['snippet'] = code_snippet
+        ctx['comments'] = code_snippet.get_comments().fetch(5)
         return ctx
 
 snippet_detail = SnippetDetail.as_view()
 
 
+def new_snippet(request):
+    "Creates a new code snippet"
+    template_name = 'snippets/edit.html'
+
+    if request.method == 'POST':
+        form = CodeSnippetForm(request.POST)
+        if form.is_valid():
+            snippet = CodeSnippet()
+            snippet = copy_snippet_from_form(form, snippet)
+            snippet.put()
+            index_snippet_with_search(snippet)
+            
+            return HttpResponseRedirect(reverse(
+                'snippets:snippet-detail',
+                kwargs={'snippet_id':snippet.key().id()}
+            ))
+    else:
+        form = CodeSnippetForm()
+
+    ctx = {}
+    ctx['form'] = form
+    ctx['form_action'] = reverse('snippets:new-snippet')
+
+    return render_to_response(
+        template_name,
+        context_instance=RequestContext(request, ctx)
+    )
+
+
 def edit_snippet(request, snippet_id=None):
-    "Edit or create a code snippet"
-    # TODO: tidy this shit up, it's mainly all left over from testing
+    "Edits an existing code snippet"
+ 
+    if snippet_id is None:
+        raise Http404()
 
     template_name = 'snippets/edit.html'
 
-    ctx = {}
+    if request.method == 'POST':
+        form = CodeSnippetForm(request.POST)
+        if form.is_valid():
+            snippet = get_snippet_or_404(snippet_id)
+            snippet = copy_snippet_from_form(form, snippet=snippet)
+            snippet.put()
+            index_snippet_with_search(snippet)
 
-    if snippet_id is not None:
-        form_action = reverse(
-            'snippets:edit-snippet',
-            kwargs={'snippet_id':snippet_id}
-        )
+            return HttpResponseRedirect(reverse(
+                'snippets:snippet-detail',
+                kwargs={'snippet_id':snippet.key().id()}
+            ))
     else:
-        form_action = reverse('snippets:new-snippet')
-
-    ctx['form_action'] = form_action
-
-    if request.method == 'GET':
         initial_data = {}
-
-        if snippet_id is not None:
-            # TODO: use `get_snippet_or_404_on_error` here
-            snippet = CodeSnippet.get_by_id(int(snippet_id))
-            if snippet is None:
-                raise Http404()
-            initial_data.update(db.to_dict(snippet))
-
+        snippet = get_snippet_or_404(snippet_id)
+        initial_data.update(db.to_dict(snippet))
         form = CodeSnippetForm(initial=initial_data, auto_id=False)
-        ctx['form'] = form
 
-        return render_to_response(
-            template_name,
-            context_instance=RequestContext(request, ctx)
-        )
-
-    form = CodeSnippetForm(request.POST)
-
-    if form.is_valid():
-        if snippet_id is None:
-            # We're creating a snippet
-            snippet = CodeSnippet()
-        else:
-            snippet = get_snippet_or_404_on_error(snippet_id)
-            if snippet is None:
-                raise Http404()
-
-        for field_name, clean_val in form.cleaned_data.items():
-            setattr(snippet, field_name, clean_val)
-
-        current_user = get_current_user()
-        if not snippet.is_saved():
-            snippet.creator = current_user
-        snippet.modifier = current_user
-
-        key = snippet.put()
-
-        snippet_doc = CodeSnippetDocument(
-            doc_id=key.id(),
-            title=snippet.title,
-            code=snippet.code,
-            language_id=snippet.language,
-            language_readable=snippet.get_language(),
-            creator_email=snippet.creator.email
-        )
-
-        index = Index(name='snippets')
-        index.add(snippet_doc)
-
-        return HttpResponseRedirect(
-            reverse('snippets:snippet-detail', kwargs={'snippet_id':key.id()})
-        )
-
+    ctx = {}
     ctx['form'] = form
+    ctx['form_action'] = reverse(
+        'snippets:edit-snippet',
+        kwargs={'snippet_id':snippet_id}
+    )
 
     return render_to_response(
         template_name,
