26f9eea517f3404149d1b6f62730f666d5bce057
Martin Allison <martin@potatolondon.com>
2012-07-24 15:49:58 +0100

    Fix using old get_current_user

--- snippets/context_processors.py
+++ snippets/context_processors.py
@@ -2,12 +2,12 @@ from google.appengine.api import users
 
 from django.core.urlresolvers import reverse
 
-from views import get_current_user
+from models import User
 
 
 def put_user_in_context(request):
     ctx = {}
-    ctx['user'] = get_current_user()
+    ctx['user'] = User.get_current()
     ctx['login_url'] = users.create_login_url(request.path_info)
     ctx['logout_url'] = users.create_logout_url(reverse('snippets:home'))
     return ctx
