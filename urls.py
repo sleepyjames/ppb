988f8e7dcfd589fd68552712f9071b3495def084
Martin Allison <martin@potatolondon.com>
2012-08-06 00:40:26 +0100

    Include i18n URLs for language switching view

--- urls.py
+++ urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, url, include
 urlpatterns = patterns(
     '',
     (r'', include('snippets.urls', namespace='snippets'),),
+    (r'^i18n/', include('django.conf.urls.i18n'),),
 )
