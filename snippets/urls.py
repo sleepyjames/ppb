bde4164c59cae0b3c27a68b0439ea233e59f9ea3
Martin Allison <martin@potatolondon.com>
2012-08-06 00:37:02 +0100

    Add in debug URLs again for the minute

--- snippets/urls.py
+++ snippets/urls.py
@@ -8,5 +8,8 @@ urlpatterns = patterns(
     url(r'^snippet/new/$', 'views.new_snippet', {}, name='new-snippet'),
     url(r'^snippet/(?P<snippet_id>\d+)/edit/$', 'views.edit_snippet', {},
         name='edit-snippet'),
+
+    url(r'^_purge/$', 'views._purge', {}, name='purge'),
+    url(r'^_reindex/$', 'views._reindex', {}, name='reindex'),
 )
 
